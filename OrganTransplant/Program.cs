d3b000450f3a15e630eddfea8ded886a0ba60631
Chris <oscar.sletvold@gmail.com>
2024-05-27 10:47:54 +0200

    Add project files.

---
+++ OrganTransplant/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace OrganTransplant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oppretter objekter for Bernt (pasienten) og Kåre (donoren)
+            Person bernt = new Person("Bernt");
+            Person kåre = new Person("Kåre");
+
+            Organ nyre = new Organ("Nyre");
+
+            // Starter transplantasjonsprosessen
+            Console.WriteLine($"En akutt ulykke har skjedd, og {bernt.Name} trenger en ny {nyre.Type}!");
+
+            Console.WriteLine($"{kåre.Name} frivillig melder seg som donor for å redde {bernt.Name}.");
+
+            Console.WriteLine("Gjennomfører nyretransplantasjon...");
+            Transplant(bernt, kåre, nyre);
+
+            Console.WriteLine($"Nyretransplantasjonen er gjennomført vellykket! {bernt.Name} har nå en ny {nyre.Type} og er i bedring.");
+        }
+
+        static void Transplant(Person recipient, Person donor, Organ organ)
+        {
+            Console.WriteLine($"{donor.Name} gir villig bort en {organ.Type} til {recipient.Name}.");
+        }
+    }
+}
